0cb87265d998c55c7fec7aab81fa7024d6c75610
ayberkgcr <deathgateway.ag@gmail.com>
2022-04-07 08:41:55 +0300

    fetching

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  useEffect(async () => {
+
+    const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+    const json = await response.json();
+    console.log(json)
+
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" >
+      <main>
+
+
+
+        <div className='search-box'>
+          <input type="text"
+            className='search-bar'
+            placeholder='Search Coin'
+          // onChange={e=>fonksiyon(e.target.value)}
+          // value={query}
+          // onKeyPress={search}
+
+
+          />
+        </div>
+
+        {/* <tbody>
+          {coins.map((coins) => {
+            return (
+              <tr key={coins.name}>
+                <td>{coins.id}</td>
+
+              </tr>
+            );
+          })}</tbody> */}
+
+
+
+      </main>
+
+
+
+
     </div>
   );
 }
