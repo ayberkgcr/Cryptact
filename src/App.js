05f6926a58615e80713cd472ec1520d0bf9b6da1
ayberkgcr <deathgateway.ag@gmail.com>
2022-04-10 07:20:30 +0300

    bootstrap ve fetching data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 import './App.css';
 
 import React, { useState } from "react";
+import { ListGroup, Card, ListGroupItem } from 'react-bootstrap';
 
+// const linked={
+//   link:"https://www.coingecko.com/en/coins/"
 
-// simple/price?ids=bitcoin&vs_currencies=usd
+// }
+
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=terra-luna&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 const api = {
-  base: "https://api.coingecko.com/api/v3/coins/"
+  // base: "https://api.coingecko.com/api/v3/coins/"
+  base: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids="
 }
 function App() {
-  // const [coins, setCoins] = useState([]);
-  // const [filteredCoins, setFilteredCoins] = useState(coins);
-  // useEffect(async () => {
-
-  //   const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=1h");
-  //   const data = await response.json();
-  //   console.log(data)
-  //   return response.json();
-  //https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd
-
-
-  // }, []);
-  //https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd
-
+  //Use State ********************************************************************************************************************
   const [query, setQuery] = useState('');
-  const [coins, setCoins] = useState({});
+  const [coins, setCoins] = useState([]);
+
+  //Use State XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+  //Arama  ********************************************************************************************************************
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}${query}` )
+      fetch(`${api.base}${query}&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
         .then(res => res.json())
         .then(result => {
-          setCoins(result);
+          setCoins(result[0]);
           setQuery('');
           console.log(result);
-          
+
         });
     }
   }
+  //Arama  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 
   return (
 
@@ -47,7 +47,7 @@ function App() {
       <main>
 
 
-
+        {/* //Search Box XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */}
         <div className='search-box'>
           <input type="text"
             className='search-bar'
@@ -58,29 +58,68 @@ function App() {
 
 
           />
-
-          
-          
         </div>
-        <div className='name'>Coin Name               :{coins.name}</div>
-        <div className="rank">Coin Gecko Rank : {coins.market_cap_rank}</div>
-        <div className='symbol'> Symbol            : {coins.symbol}</div>
-        {/* <div class="Genesis Date"> Genesis Date          : {coins.market_data.ath[0]} </div>  */}
-
-        
-
-        
-        
+        {/* SearchBox XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */}
 
 
-       
+        {/* Searched Coin Data List XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */}
+
+
+        {/* <ListGroup className=''  as="ul">
+          <ListGroup.Item as="li" disabled>
+            Coin Name               :{coins.name}
+          </ListGroup.Item>
+          <ListGroup.Item as="li"> 
+          Symbol            : {coins.symbol}
+          </ListGroup.Item>
+          <ListGroup.Item as="li">
+            Coin Gecko Rank : {coins.market_cap_rank}
+          </ListGroup.Item>
+          
+          <ListGroup.Item as="li"> 
+          Community Score: {coins.community_score}
+          </ListGroup.Item>
+          
+        </ListGroup> */}
+
+        {/*Searched Coin Data List XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */}
+
+
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" />
+          <Card.Body>
+            <Card.Title>{coins.name}</Card.Title>
+            <Card.Text>
+
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Coin Gecko Rank : {coins.image}</ListGroupItem>
+
+            <ListGroupItem>Symbol            : {coins.symbol}</ListGroupItem>
+            <ListGroupItem>Community Score: {coins.community_score}</ListGroupItem>
+            <ListGroupItem>kat: {coins.description}  </ListGroupItem>
+
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="https://www.coingecko.com/en/coins/"></Card.Link>
+
+          </Card.Body>
+        </Card>
+
+
+
+
+
+
+
 
 
 
       </main>
 
 
-      
+
 
     </div>
   );
